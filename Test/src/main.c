a1aec98fef15550dff2e8865f4890fa2b6c39d9a
Kuro10 <trantronghieu97@gmail.com>
2018-11-09 15:10:53 +0100

    Update test pour le servo moteur.

--- Test/src/main.c
+++ Test/src/main.c
@@ -1,22 +1,37 @@
-#include "stm32f10x.h"
-#include "GPIO.h"
+#include "stm32f10x.h" 
+ 
+//int TimerInit(TIM_TypeDef *timer, int arr, int psc);
 
+int LED; 
 int ok=0;
 /* Fichier de la couche "application" */
 void Ma_Fonction_IT ( void )
 {
 /* Le code à exécuter pendant l’interruption */
-	
-	if( !ok && GPIOB->IDR & (1 << 5)){
+	/*
+	LED = GPIOA->ODR>>5;
+	if (LED==0)
+	{
+		//allumer la LED
+		GPIOA->ODR |= (0x1<<5);
+	}
+	else
+	{
+		//eteindre la LED
+		GPIOA->ODR &= ~(0x1<<5);
+	}
+	*/
+	if( !ok && GPIOC->IDR & (1 << 10)){
 		ok=1;
 		TIM2->CCR1 = 144;
 	}
-	else if (ok && !(GPIOB->IDR & (1 << 5))){
+	else if (ok && !(GPIOC->IDR & (1 << 10))){
 		ok=0;
-		TIM2->CCR1 = 0;
+		TIM2->CCR1 = 0 ;
 	}
 	TIM2->SR &= ~0x1;
 }
+
 
 /* Fichier de la couche pilote */
 void (* pFnc) (void) ; /* déclaration d’un pointeur de fonction */
@@ -33,42 +48,80 @@ void TIM2_IRQHandler (void){
 }
 
 
+
+
 int main (void)
 {	
+
+	/* TP3 : Timer */
+
+		/*
+	//Activer l'horloge locale de TIM2
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	//Activer counter
+	TIM2->CR1 |= (1<<0);
+	
+	//TimerInit(TIM2,99,5759);
+	TIM2->ARR = 0x63;
+	TIM2->PSC = 0x167F;
 	
 	//Configurer l'interruption de TIM2
 	Init_periph(Ma_Fonction_IT);
 	TIM2->DIER |= (1<<0);
 	NVIC->ISER[0] |= (1<<28);
 	NVIC->IP[28] |= (uint8_t)(0x2<<4);
-	
-	//Configurer la LED PA.5 en sortie et en mode alternate function
+ 
+	//Configurer la LED PA.5 en sortie
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	//alternate push-pull
-	GPIOA->CRL &= ~(0xF<<(4*5));
-	GPIOA->CRL |= (0xA<<(4*5));
- 	
+	GPIOA->CRL &= ~(0xF<<20);
+	GPIOA->CRL |= (0x1<<20);
+	
+	//TimerStart(TIM2);
+	*/
+	
+	
+	
 	//Activer l'horloge locale de TIM2
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	//Activer counter / Lancer le timer
+	TIM2->CR1 |= TIM_CR1_CEN;
 	
 	//Configurer TIM2 avec periode de 20ms
 	TIM2->ARR = 1439;
 	TIM2->PSC = 999;
-
-	//configurer le canal CH0 d'un timer en mode PWM
+	
+	//Configurer l'interruption de TIM2
+	Init_periph(Ma_Fonction_IT);
+	TIM2->DIER |= (0x1<<0);
+	NVIC->ISER[0] |= (1<<28);
+	NVIC->IP[28] |= (uint8_t)(0x2<<4);
+	
+	//configurer le canal CH1 d'un timer en mode PWM
 	//il faut affecter la valeur 0b110 au bits OC1M dans le registre CCMR1 du TIM
 	TIM2->CCMR1 &= ~TIM_CCMR1_OC1M_0;
 	TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
-	//sur le CH1
+	TIM2->CCMR1 |= TIM_CCMR1_OC1PE;
+	//sur le CH1 (broche PA.0)
 	TIM2->CCER |= TIM_CCER_CC1E;
 	
 	//Pour l'instant, fixons la duree de l'impulsion a 144 (10%)
 	TIM2->CCR1 = 144;
 	
-	//Activer counter / Lancer le timer
-	TIM2->CR1 |= TIM_CR1_CEN;
+	//Configurer le button PC.10 en entree 
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH &= ~(0xF<<(4*2));
+	GPIOC->CRH |= (0x4<<(4*2));
 	
-	while(1) {
+	//Configurer la LED PA.0 en sortie et en mode alternate function
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	//alternate push-pull
+	GPIOA->CRL &= ~(0xF<<(4*0));
+	GPIOA->CRL |= (0xB<<(4*0));
+	
+  // boucle de traitement
+		while(1)
+    {  
 
-	}
+    }
+
 }
