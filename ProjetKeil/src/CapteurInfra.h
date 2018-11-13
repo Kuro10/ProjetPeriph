c1c057a6ae8238bb9bb8f0f6a48ef5068f5d05d9
Pierre Marigo <pierre.mrg@gmail.com>
2018-11-13 16:30:31 +0100

    Gestion de la fonction principale par un timer sur interruption

--- ProjetKeil/src/CapteurInfra.h
+++ ProjetKeil/src/CapteurInfra.h
@@ -4,19 +4,18 @@
 /* Permet d'initialiser le convertisseur A > D. */
 void initADC(void);
 
-
-/* Renvoie la valeur du voltage stockée.
+/*
+ * Renvoie la valeur du voltage stockée.
  * Une nouvelle mesure est effectuée à chaque appel.
  * @return Valeur du voltage stockée
  */
 int getVoltage(void);
 
-
-
-
-
-// TODO
-// Calcule la distance au capteur (en fonction du voltage retourné)
+/*
+ * Renvoie la distance au capteur
+ * Elle est calculée en fonction du voltage retourné (expérimental !)
+ * @return Valeur de la distance calculée
+ */
 int getDistance(void);
 
 #endif
